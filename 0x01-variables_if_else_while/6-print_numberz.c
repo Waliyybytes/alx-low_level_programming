292f0b6d9102a04e9f45ef557aa161c1e49b3c7b
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-03 15:49:44 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point , prints numbers
+ *
+ * Return: Always zero(success)
+ */
+
+int main(void)
+{
+
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+
+}
