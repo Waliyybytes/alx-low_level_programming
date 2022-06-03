f5bf9bc1a6264b84a3db944c58068c7b961f49ff
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-03 15:45:31 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point , prints numbers
+ *
+ * Return: Always zero(success)
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+
+}
