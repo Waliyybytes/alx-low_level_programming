77938658288f49803f390cdb5cac58349d87f94e
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-03 16:13:34 -0700

    print hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point , prints hexadecimal numbers (lower case)
+ *
+ * Return: Always zero(success)
+ */
+
+int main(void)
+{
+
+	int i = 48;
+	int j = 97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++
+	}
+	putchar(10);
+
+	return (0);
+
+}
