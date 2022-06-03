aeeafe34f01cadffdccb98208095019074c3e9a5
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-03 15:04:31 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point , prints alphabets
+ *
+ * Return: Always zero(success)
+ */
+
+int main(void)
+{
+
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+
+	}
+	putchar(10);
