70d6a732898dd6d8dfc3e79fb1e414b89b0aa4be
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-02 18:35:27 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Using puts to print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
