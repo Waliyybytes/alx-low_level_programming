126ee14ec59cce5d28fea29b5a73371ee6e8de29
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-03 22:52:08 -0700

    combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point , prints  three digits combinations
+ *
+ * Return: Always zero(success)
+ */
+
+int main(void)
+{
+	int k = 48;
+
+	while (k < 56)
+	{
+		int j = 49;
+
+		while (j < 57)
+		{
+
+			int i = 50;
+
+			while (i < 58)
+			{
+				if ((i + j) != (j + k) &&  (j + k) != (k + i) && (i != j) && (j != k) &&
+						(k != i) && (k < j) && (j < i))
+				{
+					putchar(k);
+					putchar(j);
+					putchar(i);
+					if (k == 58)
+						breakk;
+					putchar(44);
+					putchar(32);
+
+				}
+				i++;
+			}
+			j++;
+		}
+		k++;
+	}
+	putchar(10);
+	return (0);
+
+}
