530da82050e4eb29a8332e87baa6163d8744bdb9
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-03 17:51:19 -0700

    print comb 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point , prints combinations of double digits
+ *
+ * Return: Always zero(success)
+ */
+
+int main(void)
+{
+	int k=0;
+
+	while(k < 8)
+	{
+
+		int i = 49+k;
+		int j = 48+k;
+
+		while (i < 58)
+		{
+			
+			if (j != i)
+			{
+				putchar(j);
+				putchar(i);
+			}
+			i++;
+			if (k == 8)
+				break;
+			putchar(44);
+			putchar(32);
+		}
+		k++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
