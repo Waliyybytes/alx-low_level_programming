e84fc4cad4b636aed481e1c8bea6146aa261ade4
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-07 16:18:59 -0700

    print alphabetsx10 in low case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,13 @@
 
 void print_alphabet_x10(void)
 {
-	for (int j = 0; j < 10; j++)
+	int j;
+
+	for (j = 0; j < 10; j++)
 	{
-		for (int i = 97; i < 123; i++)
+		int i;
+
+		for (i = 97; i < 123; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
