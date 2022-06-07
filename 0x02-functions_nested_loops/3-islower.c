2d39ae2400dfefdc82daed1ed1147aa0e14c65ac
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-07 16:42:22 -0700

    check lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,14 @@
 /**
  * _islower- this is a function to check lower case
  *
+ * @c: character to be checked
  * Return: 1 is success
  *
  */
 
-int _islower(int c);
+int _islower(int c)
 {
-	if(islower(c))
+	if (islower(c))
 		return (1);
 	else
 		return (0);
