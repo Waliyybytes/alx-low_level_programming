ec931c8fac8c7468a1991aa166d50efd7e20b65c
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-08 03:11:46 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,14 @@ void times_table(void)
 		for (i = 0; i < 10; i++)
 		{
 			int prod = i * j;
+
 			if (prod > 9)
 				_putchar('0' + prod / 10);
 			_putchar('0' + prod % 10);
 			if (i != 9)
 				_putchar(44);
 			_putchar(32);
-			if (!( j * (i+1) > 9))
+			if (!(j * (i + 1) > 9))
 				_putchar(32);
 		}
 		_putchar(10);
