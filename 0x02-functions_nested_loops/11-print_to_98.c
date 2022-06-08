3d84ac7a5a06ebf97c8f910bfa42a0812b627491
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-08 04:12:17 -0700

    print num to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,16 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (;n < 99;n++)
 			printf("%d, ",n);
 	}
-	if ( n> 98)
+	else if (n > 98)
 	{
 		for (;n > 97;n--)
 			printf("%d, ",n);
 	}
+	else
+		printf(98);
 }
