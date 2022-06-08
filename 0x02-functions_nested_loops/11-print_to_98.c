451f73e2da385f634ebc9427072aaf246f6ef5bf
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-08 07:04:39 -0700

    print n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,20 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (;n < 99;n++)
-			printf("%d, ",n);
+		for (; n < 99; n++)
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
 	}
 	else if (n > 98)
 	{
-		for (;n > 97;n--)
-			printf("%d, ",n);
+		for (; n > 97; n--)
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
 	}
 	else
-		printf("%d",n);
+		printf("%d", n);
 }
