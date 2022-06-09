de063487b513d32036384171c0459dcd2411e6e2
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-08 20:57:34 -0700

    print 98 fibs

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,21 +10,21 @@
 
 int main(void)
 {
-	uintmax_t a = 1;
-	uintmax_t b = 2;
-	uintmax_t next = a + b;
+	u_int64_t a = 1;
+	u_int64_t b = 2;
+	u_int64_t next = a + b;
 	int i;
 
-	printf("%llu, %llu, %llu, ", a, b, next);
+	printf("%lu, %lu, %lu, ", a, b, next);
 	for (i = 4; i <= 98; i++)
 	{
 		a = b;
 		b = next;
 		next = a + b;
 		if (i < 98)
-			printf("%llu, ", next);
+			printf("%lu, ", next);
 		if (i == 98)
-			printf("%llu\n", next);
+			printf("%lu\n", next);
 	}
 	return (0);
 }
