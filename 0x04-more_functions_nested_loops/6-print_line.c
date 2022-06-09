64dac48f3389534f08b75ab63f5b890d301ff32f
Waliyybytes <you@example.com>
2022-06-09 18:50:37 +0100

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* print_line- this is a function to check for a digit
+*
+* @n: given length of line
+*/
+
+void print_line(int n)
+{
+	int n;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
