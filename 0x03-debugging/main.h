76cee6091d21d259c6aaf5866baadcd1da823eba
Waliyybytes <you@example.com>
2022-06-09 15:46:56 +0100

    leap year debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
 
 /* the first debugging test */
 
-void positive_or_negative(int i);
+void positive_or_negative(int);
 
 /* print largest number */
 
-int largest_number(int a, int b, int c);
+int largest_number(int, int, int);
+
+void print_remaining_days(int, int, int);
+
+int convert_day(int, int);
