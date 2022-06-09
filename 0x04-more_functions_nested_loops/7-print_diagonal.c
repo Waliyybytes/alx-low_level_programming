4959c319e8eb5b8ea0673c07a315ae408fa1444f
Waliyybytes <you@example.com>
2022-06-09 21:13:12 +0100

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* print_diagonal- this is a function to check for a digit
+*
+* @n: given length of line
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('\\');
+		_putchar('\n');
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -24,3 +24,4 @@ void more_numbers(void);
 
 void print_line(int);
 
+void print_diagonal(int);
