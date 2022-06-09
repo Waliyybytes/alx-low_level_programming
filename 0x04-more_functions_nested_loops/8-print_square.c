6df3a76139a16396a5ab9c2dbbf4ac1400ffa4dc
Waliyybytes <you@example.com>
2022-06-09 23:05:25 +0100

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* print_square- this is a function to check for a digit
+*
+* @n: given length of square
+*/
+
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			_putchar('#');
+			if (j == n)
+				_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -25,3 +25,5 @@ void more_numbers(void);
 void print_line(int);
 
 void print_diagonal(int);
+
+void print_square(int);
