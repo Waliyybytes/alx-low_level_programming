190eefcde7bfd47d830eaef7ee08d4baead5f147
Waliyybytes <you@example.com>
2022-06-13 15:12:47 +0100

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - to reset a value to 98
+ *
+ * @a: character to swap
+ * @b: character to swap
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
 /** header file on pointer array and string functions **/
 
 void reset_to_98(int *);
+
+/* to swap values of two integers */
+
+void swap_int(int *, int *);
