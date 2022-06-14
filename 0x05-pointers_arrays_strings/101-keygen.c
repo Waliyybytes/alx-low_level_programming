e1a0eae8e4c35e4a11bd1aa0d031d4c2302321e4
Waliyybytes <you@example.com>
2022-06-14 22:15:55 +0100

    passcode generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main- generate a 14 key password
+ *
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	int password_length = 14;
+
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+
+	srand(time(NULL));
+	for (int i = 0; i < password_length; i++)
+	{
+		printf("%c", list[rand() % (sizeof(list) - 1)]);
+	}
+	printf("\n");
+	return (0);
+
+}
