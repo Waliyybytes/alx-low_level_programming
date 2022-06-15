028c8baf5835b2c998c652ee1c0aee7a3d732e33
Waliyybytes <you@example.com>
2022-06-15 22:26:02 +0100

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <main.h>
+#include "main.h"
 /**
  * *_strcat - function to concatenate strings
  *
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
 /* This is more on pointers, arrays and srings */
 
 char *_strcat(char *, char *);
+
+char *_strncat(char *, char *, int);
+
+char *_strncpy(char *, char *, int);
+
+int _strcmp(char *s1, char *s2);
