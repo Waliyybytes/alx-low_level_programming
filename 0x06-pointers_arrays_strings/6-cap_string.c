2a93d43f0d1d4bc9738d32bd9614c1607f113d94
Waliyybytes <you@example.com>
2022-06-16 11:21:10 +0100

    captilalize strings  via separator

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * *cap_string- function to change to uppercase
+ *
+ * @s: string to captilalize
+ * Return: returns a pointer to the string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	int len = strlen(len);
+
+	for (i = 0; i < len; i++)
+	{
+		if (!isalpha(s[i]))
+		{
+			for (int j = 0; j < 13; j++)
+			{
+				if (sep[j] == s[i])
+					s[i + 1] = (char)toupper(s[i + 1]);
+			}
+		}
+		if (s[i] == '\t')
+			s[i] = ' ';
+		if (isalpha(s[0]))
+			s[0] = (char)toupper(s[0]);
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,5 @@ int _strcmp(char *, char *);
 void reverse_array(int *, int );
 
 char *string_toupper(char *);
+
+char *cap_string(char *);
