81a05cebfa101ce08e8a7cc85f18e1865f7ac1d9
Waliyybytes <you@example.com>
2022-06-17 10:53:25 +0100

    fixed long numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 #include <ctype.h>
+#include <limits.h>
 /**
  * print_number- this is a function to check for a digit
  *
@@ -11,30 +12,42 @@ void print_number(int n)
 {
 	int i;
 	int cuns;
+	int rem = 0;
+	int quot;
 
 	if (n > 0 || n < 0)
 	{
-		int a = n;
-
-		if (a < 0)
+		if (n > 999999999 || n < -1000000000)
 		{
-			n = -n;
-			_putchar('-');
+			quot =  INT_MAX / 10;
+			rem = INT_MAX / 10;
+			if (n < 0)
+				putchar('-');
+			cuns = 100000000;
+			n = quot;
 		}
+		else
+		{
+			int a = n;
 
-		for (i = 1; i <= n; i *= 10)
-			cuns = i;
-
+			if (a < 0)
+			{
+				n = -n;
+				_putchar('-');
+			}
+			for (i = 1; i <= n; i *= 10)
+				cuns = i;
+		}
 		while (cuns != 0)
 		{
 			_putchar('0' + n / cuns);
 			n %= cuns;
 			cuns /= 10;
 		}
+		_putchar('0' + (n > 0 ? rem : rem + 1));
 	}
 	else
 		_putchar('0' + n % 10);
-
 }
 
 
