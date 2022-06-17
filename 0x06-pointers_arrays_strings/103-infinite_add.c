90447d25e1e72b844b24993e8273438e152e3ca2
Waliyybytes <you@example.com>
2022-06-17 23:00:09 +0100

    infinity

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include  <string.h>
+/**
+ * *infinite_add -to add inifinite numbers
+ * @n1: string 1
+ * @n2: string 2
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: returns a result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0;
+	int carry = 0;
+	int index = 0;
+	int next = 0;
+	char sum[];
+
+	reverseArray(n1)
+	reverseArray(n2);
+	while (1)
+	{
+		int sum_two = carry;
+
+		if (n1[index])
+		{
+			sum_two += n1[index] - '0';
+			index++;
+		}
+		if (n2[next])
+		{
+			sum_two += n2[next] - '0';
+			next++;
+		}
+		sum[i] = sum_two % 10 + '0';
+		i++;
+		carry = sum_two / 10;
+		if (n1[index] == 0 && n2[next] == 0)
+			break;
+	}
+	if (carry)
+		sum[i] = carry + '0';
+	reverse_array(sum);
+	if (strlen(sum) > size_r)
+		return (0);
+	else
+		return (sum);
+
+
+}
---
+++ 0x06-pointers_arrays_strings/reverse.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - function to reverse elements of an array
+ *
+ * @a: array to reverse
+ * @n: number of elements in array
+ * Return: returns nothing
+ */
+
+void reverse_array(char *a)
+{
+	int i;
+	int len = n;
+	int temp;
+
+	for (i = 0; i < len; i++)
+	{
+		temp = a[i];
+		a[i] = a[len - 1];
+		a[len - 1] = temp;
+		len--;
+	}
+
+}
