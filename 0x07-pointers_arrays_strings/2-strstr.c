3c59bef7e84f92e125ca4f30293af37ee3221f65
Waliyybytes <you@example.com>
2022-06-20 20:59:40 +0100

    locate a character

---
+++ 0x07-pointers_arrays_strings/2-strstr.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strchr - prints buffer in hexa
+ *
+ * @s: address of memory to print
+ * @c: character to to find
+ *
+ * Return: returns a pointer to s
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,5 @@
 char *_memset(char *, char, unsigned int);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
