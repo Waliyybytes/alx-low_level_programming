b70e067daba8e0bb6d4545f9504bd85e01d80efa
Waliyybytes <you@example.com>
2022-06-22 15:29:26 +0100

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints a string
+ *
+ * @s: character to s
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+	}
+	_putchar(s);
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,3 @@
 void _puts_recursion(char *);
+
+int _putchar(int);
