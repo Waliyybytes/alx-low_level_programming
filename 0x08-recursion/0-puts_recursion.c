523e417b8c3da6436ea40b6031d065f8f581bba7
Waliyybytes <you@example.com>
2022-06-22 11:03:12 +0100

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: character to s
+ * @Return: character
+ */
+
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,9 @@
+This is about recursion 
+
+We need to go deeper into this
+
+But dont forget the base ...else
+
+you will be forever stuck and now way to get out
+
+Hence think deeper
