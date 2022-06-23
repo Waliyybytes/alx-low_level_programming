f113e1d853e0aa1ba2be3a6516eb987ba820369a
Waliyybytes <you@example.com>
2022-06-23 15:56:02 +0100

    bugs

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -30,7 +30,7 @@ int compare_strings(char *s1, char *s2, int i)
 		return (s1[i] != '\0' && s2[i] != '\0' && compare_strings(s1, s2, i + 1));
 	if (s2[i] == '\0')
 		return (s1[i] == '\0');
-	if (s1[i] != s2[i] || s2[i] == '*')
+	if (s1[i] != s2[i] || s2[i] != '*')
 		return ((s1[i] != '\0' || s2[i] != '\0') &&  compare_strings(s1, s2, i + 1));
 }
 
