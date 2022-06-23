1bd186866e651d85da9f33c9fac21b36a63644ce
Waliyybytes <you@example.com>
2022-06-23 15:05:32 +0100

    wildcards comparisons

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -31,7 +31,7 @@ int compare_strings(char *s1, char *s2, int i)
 	if (s2[i] == '\0' && s1[i] == '\0')
 		return (1);
 	if (s2[i] == '*')
-		return (s1[i] != '\0' && compare_strings(s1, s2, i + 1));
+		return (s1[i] != '\0' || compare_strings(s1, s2, i + 1));
 }
 
 
