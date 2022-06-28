fbe4ab165fe7e8b17e48bfcfdf397011ff51deeb
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-28 23:14:53 +0100

    string_memory_duplicates

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 
 	if (str != NULL)
 	{
-		copy = (char *)malloc(sizeof(strdup(str)-1));
+		copy = (char *)malloc(strlen(str) * sizeof(char));
 		if (copy == NULL)
 			return (NULL);
 		copy = strdup(str);
