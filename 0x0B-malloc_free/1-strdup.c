8d562e78f05b3eb12aa48aa9d7bc0ff52d19fd63
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-28 23:02:30 +0100

    string duplcates

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strdup - to duplicate a string
+ * @str: string duplicated
+ * Return: NULL or a pointer to the array
+ */
+
+char *_strdup(char *str);
+{
+	unsigned int i;
+	char *copy;
+
+	if (str != NULL)
+	{
+		copy = (char *)malloc(sizeof(strdup(str));
+		if (copy == NULL)
+			return (NULL);
+		copy = strdup(str);
+	}
+	else
+		return (NULL);
+
+	return (copy);
+}
+
+
