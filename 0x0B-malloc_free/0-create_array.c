03c1b086032f64d750b17f6e7ba9a7788b8fe9fa
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-28 22:28:57 +0100

    dynamically_allocated_arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	{
 		array = (char *)malloc(size * sizeof(char));
 		if (array == NULL)
-			return NULL;
+			return (NULL);
 
 		for (i = 0; i < size; i++)
 			array[i] = c;
