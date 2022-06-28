5f2238f55878c27f279e4e6e412213e0e5b53dd0
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-28 23:20:19 +0100

    string_duplicates_dynamics

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 
 	if (str != NULL)
 	{
-		copy = (char *)malloc(sizeof(str) * sizeof(char));
+		copy = (char *)malloc(sizeof(str));
 		if (copy == NULL)
 			return (NULL);
 		copy = strdup(str);
