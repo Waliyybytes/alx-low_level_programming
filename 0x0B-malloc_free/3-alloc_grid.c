982641d6fa4b6cd0f1c5d5d01745e65f990ef512
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-29 01:27:43 +0100

    2-dimension dynamics

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * alloc_grid - to creat two dimensional array
+ * @width: array width
+ * @height: array height
+ * Return: NULL or a pointer to the new string formed
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *mesh, **grid;
+	int i, w, h;
+
+	mesh = (int *)malloc(sizeof(width * height);
+	if (mesh == NULL)
+		return (NULL);
+	i = 0;
+	while (mesh[i])
+	{
+		for (h = 0; h < height; h++)
+		{
+			for (w = 0; w < width; w++)
+				grid[h][w] = 0;
+		}
+		i++;
+	}
+	return (grid);
+}
+
+
