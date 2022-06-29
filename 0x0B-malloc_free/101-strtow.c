b7f3383724206e6df05b2791ec6e59197553d62d
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-29 23:29:44 +0100

    Splitting strings

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,40 +3,6 @@
 #include "main.h"
 #include <string.h>
 #include <ctype.h>
-/**
- * strtow - split a strings to words
- * @str: array of args
- * Return: pointer or NULL
- */
-char **strtow(char *str)
-{
-	int i = 0, j = 0, k = 0, len = 0;
-	char **ptr;
-	int l_idx = 0, c_idx = 0;
-
-	while (str[i])
-	{
-		if (str[i + 1] == ' ')
-			len++;
-		i++;
-	}
-	ptr = (char **)malloc(sizeof(char *) * (len + 2));
-	if (ptr == NULL)
-		return (NULL);
-	while (str[i])
-	{
-		if (str[i + 1] == ' ')
-		{
-			c_idx = i + 1;
-			ptr[j] = (char *)malloc(sizeof(char) * (c_idx - l_idx + 1));
-			l_idx = c_idx + 1;
-			j++;
-		}
-		i++;
-	}
-	ptr = fix_string(str)
-	return (ptr);
-}
 
 /**
  * fix_string - to fix charcaters
@@ -64,4 +30,38 @@ char **fix_string(char *str)
 	}
 	return (ptr);
 }
+/**
+ * strtow - split a strings to words
+ * @str: array of args
+ * Return: pointer or NULL
+ */
 
+char **strtow(char *str)
+{
+	int i = 0, j = 0, k = 0, len = 0;
+	char **ptr;
+	int l_idx = 0, c_idx = 0;
+
+	while (str[i])
+	{
+		if (str[i + 1] == ' ')
+			len++;
+		i++;
+	}
+	ptr = (char **)malloc(sizeof(char *) * (len + 2));
+	if (ptr == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i + 1] == ' ')
+		{
+			c_idx = i + 1;
+			ptr[j] = (char *)malloc(sizeof(char) * (c_idx - l_idx + 1));
+			l_idx = c_idx + 1;
+			j++;
+		}
+		i++;
+	}
+	ptr = fix_string(str);
+	return (ptr);
+}
