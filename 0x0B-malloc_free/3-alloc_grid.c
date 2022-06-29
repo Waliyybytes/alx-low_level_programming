cb45d13fca6252ce4fe9b6d8a5f912c7abd59392
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-29 03:30:03 +0100

    grids

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,8 +16,8 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(height));
-	*grid = (int *)malloc(sizeof(width));
+	grid = (int **)malloc(sizeof(width));
+	*grid = (int *)malloc(sizeof(height));
 	if (*grid == NULL || grid == NULL)
 		return (NULL);
 	for (h = 0; h < height; h++)
