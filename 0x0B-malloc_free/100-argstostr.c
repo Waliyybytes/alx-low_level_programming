c7a4ed486edd3cf536386ace3044958b5c74fc66
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-29 18:10:58 +0100

    CLI args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,31 +13,29 @@ char *argstostr(int ac, char **av)
 {
 	int i;
 	size_t j;
-	char **ptr;
-	char *str = NULL;
+	char *ptr;
+	int len = 0;
 	int count = 0;
 
-	ptr = (char **)malloc(sizeof(char) * ac);
-
 	i = 0;
-	while (i < ac)
-		ptr[i] = (char *)malloc(sizeof(char) * (strlen(av[i])));
+	while (av[i])
+	{
+		len = len + strlen(av[i]);
+	}
+
+	ptr = (char *)malloc(sizeof(char) * (len + ac - 1));
 
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i]; j++)
-			ptr[i][j] = av[i][j];
-		ptr[i][strlen(av[i])] = '\n';
-
-	}
-	for (i = 0; ptr; i++)
-	{
-		for (j = 0; ptr[i]; j++)
-		{
-			str[count] = ptr[i][j];
+		{	
+			ptr[count] = av[i][j];
 			count++;
 		}
+		ptr[count] = '\n';
+		count++;
+
 	}
 
-	return (*ptr);
+	return (ptr);
 }
