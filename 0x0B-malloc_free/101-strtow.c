63d62d5f4b06d414e3ee7964073b65aa34ab2476
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-29 21:59:31 +0100

    String_to_Words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * strtow - split a strings to words
+ * @str: array of args
+ *
+ * Return: pointer or NULL
+ */
+
+char **strtow(char *str);
+{
+	int i, j, k, len;
+	char **ptr;
+	int l_idx = 0, c_idx = 0;
+
+	if (str == "" || str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == " ")
+			len++;
+		i++;
+	}
+	ptr = (char **)malloc(sizeof(char *) * (len + 2));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == " ")
+		{
+			c_idx = i;
+			ptr[j] = (char *)malloc(sizeof(char) * (c_idx - l_idx + 1));
+			l_idx = c_idx + 1;
+			j++;
+		}
+		i++;
+	}
+	k = 0;
+	while (str[k])
+	{
+		for (i = 0; ptr; i++)
+		{
+			for (j = 0; ptr[i]; j++)
+			{
+				if (str[k] != " ")
+					ptr[i][j] = str[k];
+				else
+					ptr[i][j] = '\0';
+				k++;
+			}
+		}
+	}
+	return (ptr);
+}
