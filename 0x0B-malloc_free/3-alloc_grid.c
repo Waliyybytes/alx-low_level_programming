4455ce15917657d27b42fbeb40804019567b9788
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-29 03:27:20 +0100

    grids mesh

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int *)malloc(sizeof(height));
+	grid = (int **)malloc(sizeof(height));
 	*grid = (int *)malloc(sizeof(width));
 	if (*grid == NULL || grid == NULL)
 		return (NULL);
