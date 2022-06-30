a432f0aee8a105df7139360f02760dab2c2b9384
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-30 14:02:50 +0100

    string splits

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,11 +10,10 @@
 char **strtow(char *str)
 {
 	int i = 0, k = 0, l = 0, c_idx = 0, len = 0, j = 0;
-	char **ptr;
-	size_t real_length = 0;
-	
-	if (*str == ' ')
-		return NULL;
+	char **ptr, size_t real_length = 0;
+
+	if (*str == ' ' || str == NULL)
+		return (NULL);
 	while (str[i])
 	{
 		if (str[i] == ' ' && (isgraph(str[i + 1]) || isgraph(str[i - 1])))
@@ -45,11 +44,9 @@ char **strtow(char *str)
 				i++;
 				break;
 			}
-			i++;
-			real_length++;
+			i++, real_length++;
 		}
 	}
 	ptr[len + 2] = NULL;
 	return (ptr);
 }
-
