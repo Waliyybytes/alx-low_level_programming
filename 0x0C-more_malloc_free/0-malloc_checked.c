9c3c097d5b6ff2a195828e8670411f23eff40a98
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-06-30 23:30:21 +0100

    allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+#include <limits.h>
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -9,7 +10,7 @@
 
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL)
+	if (b  >= INT_MAX)
 		exit(98);
 	return (malloc(b));
 }
