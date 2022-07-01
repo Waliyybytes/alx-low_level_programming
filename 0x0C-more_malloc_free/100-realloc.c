5a5e89b7936cf2ebeb4cc25715f14fde702b1a64
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-01 22:01:26 +0100

    building realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+#include <limits.h>
+
+/**
+ * _realloc - allocates to n elements size bytes
+ * @ptr: pointer to old array
+ * @old_size: size of old array
+ * @new_size: size of new array
+ * Return: returns a pointer or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int i = 0;
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+	if (new_size > old_size)
+	{
+		new_ptr = malloc(new_size);
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = ptr[i];
+		free(ptr);
+		return (new_ptr);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size < old_size && new_size != 0)
+	{
+		new_ptr = malloc(new_size);
+		for (i = 0; i < new_size; i++)
+			new_ptr[i] = ptr[i];
+		free(ptr);
+		return (new_ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
+}
