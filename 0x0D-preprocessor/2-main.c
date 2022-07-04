e3653581f77b3e90f81a1d373ffd5e429c466428
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-04 23:26:38 +0100

    name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main- prints name of the current file
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char *s = __FILE__;
+	int i = strlen(s);
+
+	while (s[i] != '/')
+		i--;
+	i++;
+	for (; i < strlen(s); i++)
+		_putchar(s[i]);
+	return (0);
+}
+
+
+
