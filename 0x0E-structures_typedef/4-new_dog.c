a7b7da41ce28c09f243539d3086357798a824304
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-05 11:24:36 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *jadid;
 
-	jadid = malloc(sizeof(dog_t));
+	jadid = malloc(sizeof(dog_t *));
 	if (jadid == NULL)
 		return (NULL);
 	jadid->name = name;
 	jadid->owner = owner;
+	jadid->age = age;
 
 	return (jadid);
 }
