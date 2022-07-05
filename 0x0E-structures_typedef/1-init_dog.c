88b124df6a9476a80724e7114cdba49f5b2c54ef
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-05 03:18:34 +0100

    dog initials

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-#include "main.h"
 
 /**
  * init_dog - initialise dog details
--- 0x0E-structures_typedef/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void init_dog(struct dog *, char *, float, char *);
-
-#endif
-
