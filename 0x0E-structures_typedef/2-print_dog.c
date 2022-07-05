11a70816dd749e082b59394f3186f947110e70c0
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-05 05:56:37 +0100

    dog details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", ((d->name == NULL) ? "nil" : d->name));
 
-	printf("Age: %f\n", ((d->age == 0.0) ? printf("nil\n") : d->age));
+	printf("Age: %f\n", ((d->age == 0.0) ? printf("Age: nil\n") : d->age));
 	printf("Owner: %s\n", ((d->owner == NULL) ? "nil" : d->owner));
 }
