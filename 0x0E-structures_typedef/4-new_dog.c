f210961b15c7bc807d72b46b44d9a1681c52f7ba
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-05 11:10:08 +0100

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - new dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Nothing
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *jadid;
+	jadid = malloc(sizeof(dog_t));
+	if (jadid == NULL)
+		return (NULL);
+	jadid->name = name;
+	jadid->owner = owner;
+
+	return jadid;
+}
