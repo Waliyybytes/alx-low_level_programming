8cade464520ec19b068ddbc53a7a3b8101b8f2b9
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-07 03:28:55 +0100

    searching pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 
 	if (array == NULL || cmp == NULL)
-		return(-1);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
