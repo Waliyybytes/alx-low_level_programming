cbc79e1d53baa8fb33e54cc23b2444c0f3a0185d
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-08 03:11:24 +0100

    string print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,10 +22,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s%s", va_arg(ap, char *), separator);
 		else if (i != (n - 1) && separator != NULL && va_arg(ap, char *) == NULL)
 			printf("%s%s", "(nil)", separator);
-		else if (i == (n - 1) && va_arg(ap, char *) == NULL)
-			printf("%s", "(nil)");
 		else
-			printf("%s", va_arg(ap, char *));
+			printf("%s", "(nil)");
 	}
 	printf("\n");
 	va_end(ap);
