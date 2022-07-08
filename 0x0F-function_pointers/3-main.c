660a37b5b3ebc35da488d772179e88c11ad6fda3
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-08 22:07:51 +0100

    basic calculate

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,10 +10,18 @@
  */
 
 int main(int argc, char *argv[])
-{	
+{
+	int (*fpt)(int, int);
+	
+	fpt = get_op_func(argv[2]);
 
+	if (fpt == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	if (argc == 4)
-		printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+		printf("%d\n", (*fpt)(atoi(argv[1]), atoi(argv[3])));
 	else
 	{
 		printf("Error\n");
