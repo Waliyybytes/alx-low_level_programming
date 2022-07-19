99c54ba21bc198a929c3f12289ab87631a4e710a
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-19 23:19:32 +0100

    free pointers

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * free_listint- function
+ * @head: a list_t type
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *currentNode;
+
+	if (*head == NULL)
+	{
+		free(*head);
+		return;
+	}
+	while ((*head)->next != NULL)
+	{
+		currentNode = *head;
+		*head = (*head)->next;
+		free(currentNode);
+	}
+	free(*head);
+	*head = NULL;
+}
+
