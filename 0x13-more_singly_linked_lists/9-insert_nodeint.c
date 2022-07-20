139d2ece3e2abfdcd773fef9ff04f43966c4786b
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-20 22:30:15 +0100

    insertible

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,8 +29,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (tmp == NULL)
 			return (NULL);
 	}
-	newNode->next = tmp->next;
-	tmp->next = newNode;
+	if (i == idx - 1)
+	{
+		newNode->next = tmp->next;
+		tmp->next = newNode;
+	}
 	return (newNode);
 }
 
