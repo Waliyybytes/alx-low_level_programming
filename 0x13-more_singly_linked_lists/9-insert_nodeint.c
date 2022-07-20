3c20979046c9140550c25c6a0cba5c6bd6243398
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-20 22:49:13 +0100

    insertibles

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,14 +14,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *newNode;
-	listint_t *tmp = *head;
+	listint_t *tmp;
 
 	newNode = malloc(sizeof(listint_t));
 	newNode->n = n;
 	if (newNode == NULL)
 		return (NULL);
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
+	tmp = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (tmp != NULL)
