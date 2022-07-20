2d7fd48832a3d6bb5611fec637283ceaefa84f91
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-20 22:03:36 +0100

    inserts too

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->n = n;
 	if (newNode == NULL)
 		return (NULL);
+	if (tmp == NULL)
+		return (NULL);
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (tmp->next != NULL)
