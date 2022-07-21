dde9adcb42a76e3cfd87f297aa4b1e4ea0ffd607
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 00:54:37 +0100

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -34,6 +34,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int j = strlen(b) - 1;
 	unsigned int i = 0, sum = 0;
 
+	if (b == NULL)
+		return (0);
+
 	for (i = 0; i < strlen(b); i++)
 	{
 		if (b[i] != '1' && b[i] != '0')
