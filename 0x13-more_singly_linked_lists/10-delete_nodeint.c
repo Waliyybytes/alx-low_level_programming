7e2e8724108abf4427a056064ff48e0c65d89316
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-21 03:28:30 +0100

    delete at nth

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * delete_nodeint_at_index- function
+ * @head: a list_t type
+ * @index: where to index
+ * Return: int
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0, count = 0;
+	listint_t *tmp;
+	listint_t *tmp2;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	tmp = *head;
+	tmp2 = *head;
+	while (tmp2 != NULL)
+	{	
+		tmp2 = tmp2->next;
+		count++;
+	}
+	if (index == 0)
+		*head = (*head)->next;
+	if (index > 0 && index < count)
+	{
+		for (i = 0; i < index; i++)
+        	{
+                	if (tmp->next != NULL)
+                        	tmp = tmp->next;
+        	}
+		tmp->next = tmp->next->next;
+	}
+	if (index == count)
+	{
+		while(tmp->next->next != NULL)
+			tmp = tmp->next;
+		tmp->next = NULL;
+	}
+	if (index > count)
+		return (-1);
+
+	return (1);
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,28 +14,40 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *newNode;
-	listint_t *tmp;
+	listint_t *tmp, tmp2;
 
 	newNode = malloc(sizeof(listint_t));
 	newNode->n = n;
-	newNode->next = NULL;
 	if (newNode == NULL)
 		return (NULL);
 	if (head == NULL || *head == NULL)
 		return (newNode);
+	tmp2 = *head;
 	tmp = *head;
-	if (idx > 0)
+	while (tmp2 != NULL)
+		count++;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+	}
+	if (idx < count)
 	{
 		for (i = 0; i < idx - 1; i++)
 		{
 			if (tmp != NULL)
 				tmp = tmp->next;
-			if (tmp == NULL)
-				return (NULL);
 		}
+		newNode->next = tmp->next;
+		tmp->next = newNode;
 	}
-	newNode->next = tmp->next;
-	tmp->next = newNode;
+	if (idx == count)
+	{
+		newNode->next = NULL;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newNode;
+	}
 	return (newNode);
 }
 
