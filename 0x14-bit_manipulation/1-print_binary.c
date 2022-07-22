4f2e9e4edbe9d678e6b491feeddd0be135685d31
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 02:53:19 +0100

    bin to int

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function
+ * @b: binary to convert
+ *
+ * Return: returned equivalent decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	unsigned long int n, digit, dec = 0;
+
+	if (b == NULL)
+		return (0);
+	n = stroul(b);
+	while (n != 0)
+	{
+		digit = n % 10;
+		dec += digit << i;
+		n = n / 10;
+		i++;
+	}
+	for (i = 0; i < strlen(b); i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	}
+	return (dec);
+}
