72bb82246ada30cbb6c7b988350e7ee8a7f3785e
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 18:58:55 +0100

    fixing bug

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,6 +5,25 @@
 #include "main.h"
 
 /**
+ * power - function
+ * @x: power
+ * @y: exponent
+ * Return: Return unsigned int
+ */
+
+ unsigned int power(int x , int y)
+{
+	int i;
+	int product = 1;
+	for (i = 0; i < y; i++)
+	{
+		product *= x;
+	}
+	return product;
+}
+
+
+/**
  * binary_to_uint - function
  * @b: binary to convert
  *
@@ -13,23 +32,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
-	unsigned long int n, digit, dec = 0;
-
+	unsigned long int i = 0, dec = 0;
+	unsigned long int exp = strlen(b) - 1;
+	
 	if (b == NULL)
 		return (0);
-	n = atol(b);
-	while (n != 0)
-	{
-		digit = n % 10;
-		dec += digit << i;
-		n = n / 10;
-		i++;
-	}
+
 	for (i = 0; i < strlen(b); i++)
 	{
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
+		dec += (b[i] - '0') * power(2,exp);
+                exp--;
+
 	}
 	return (dec);
 }
