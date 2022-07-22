fbcd334b56eec630410076627e93749e8939e3fb
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 02:13:22 +0100

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,47 +3,32 @@
 #include <string.h>
 #include <math.h>
 #include "main.h"
-/**
- * powint - function
- * @x: member
- * @y: member
- * Return: Return unsigned int
- */
-
-
-unsigned int powint(int x, int y)
-{
-	int i = 0;
-	unsigned int product = 1;
-
-	if (y == 0)
-		return (product);
-	for (i = 0; i < y; i++)
-		product *=  x;
-	return (product);
-}
 
 /**
  * binary_to_uint - function
- * @b: binary literal
- * Return: Return unsigned int
+ * @b: binary to convert
+ *
+ * Return: returned equivalent decimal
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int j = strlen(b) - 1;
-	unsigned int i = 0, sum = 0;
+	unsigned int n, digit, dec = 0, i = 0;
 
 	if (b == NULL)
 		return (0);
-
+	n = atoi(b);
+	while (n != 0)
+	{
+		digit = n % 10;
+		dec += digit << i;
+		n = n / 10;
+		i++;
+	}
 	for (i = 0; i < strlen(b); i++)
 	{
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
-		sum += (b[i] - '0') * powint(2, j);
-		j--;
 	}
-	return (sum);
+	return (dec);
 }
-
