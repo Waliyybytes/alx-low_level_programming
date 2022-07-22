878529406775b62247fb7016851ac35f89412762
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 03:32:26 +0100

    int to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,10 +5,9 @@
  *
  * Return: returned equivalent decimal
  */
-int print_binary(unsigned long int n)
+void print_binary(unsigned long int n)
 {
 	if (n >> 1)
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
-
 }
