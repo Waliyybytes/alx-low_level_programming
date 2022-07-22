46bd1353d107511d0517bada369377edbedc9ee1
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 16:04:09 +0100

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_bit - function
+ * @n: convert to dec
+ * @index: index position
+ * Return: returned equivalent decimal
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n  =  *n | (1 << index);
+	if (*n < 0)
+		return (1)
+	else
+		return (-1);
+}
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,4 +9,6 @@ int _putchar(int c);
 
 int get_bit(unsigned long int n, unsigned int index);
 
+int set_bit(unsigned long int n, unsigned int index);
+
 #endif
