690af5b179f83483de3f09eb1e358b89d86a5c44
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-22 13:25:47 +0100

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,15 +2,24 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_binary - function
+ * get_bit - function
  * @n: convert to dec
  * @index: index position
  * Return: returned equivalent decimal
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	n = n >> idx;
+	unsigned int tmp = n;
+	unsigned int count = 1;
 
-	return ( n & 1);
+	while (tmp >> 1)
+	{
+		count++;
+		tmp = tmp >> 1;
+	}
+	if (index > count)
+		return (-1);
+	n = n >> index;
+	return (n & 1);
 }
 
