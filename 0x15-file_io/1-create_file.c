657923d8e165250a68f6e9064d49eae97755b9c8
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-29 05:17:33 +0100

    create file fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int wrt;
 	size_t len = strlen(text_content);
 
 	fd = creat(filename, 0600);
@@ -26,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "\0";
 
-	wrt = write(fd, text_content, len);
+	write(fd, text_content, len);
 	close(fd);
 
 	return (1);
