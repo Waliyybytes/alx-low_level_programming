ccadef7452377272474fb044a959bda05f75f206
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-29 14:09:10 +0100

    copy implemenatation

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,15 +25,13 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(argv[1], O_RDONLY, 0400);
-
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fd2 == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
@@ -47,15 +45,11 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
+	if (countRead == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	cl2 = close(fd2);
 	cl1 = close(fd1);
 	if (cl1 < 0 || cl2 < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1 || fd2);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", fd1 || fd2), exit(100);
 	return (1);
 }
-
-
-
