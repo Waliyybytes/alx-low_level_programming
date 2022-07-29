03db6f89836f5fd6cfb432167bcaf9b6e86ac882
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-29 03:52:10 +0100

    creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * create_file - creates a file and writes to it
+ * @filename: file's name
+ * @text_content: string to write
+ * Return: number of letters it could read
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int wrt;
+	size_t len = strlen(text_content);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = creat(filename, 0600);
+
+	if (filename == NULL)
+		return (-1);
+	if (fd == -1)
+		return (-1);
+	wrt = write(fd, text_content, len);
+
+	close(fd);
+
+	if (wrt < 0)
+		return (-1);
+	return (1);
+}
+
+
+
