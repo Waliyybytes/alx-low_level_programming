845789de327670f03fa5f85c113af8d85c2ae92d
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-29 05:11:40 +0100

    creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,10 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
+		text_content = "";
 	wrt = write(fd, text_content, len);
 	close(fd);
 	if (wrt < 0)
