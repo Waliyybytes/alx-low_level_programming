3d88d012ce6472f0050530cee82c89d7ab81e1c3
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-07-29 06:41:13 +0100

    bugs on create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	fd = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	fd = creat(filename, 0600);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	if (fd == -1)
 		return (-1);
 
