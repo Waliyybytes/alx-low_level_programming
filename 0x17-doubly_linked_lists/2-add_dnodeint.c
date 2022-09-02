60584aa6ac4305f8919ea7bed21c8f42e6900d65
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-02 02:07:56 +0100

    add_node_hea

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,6 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
-
-	return (*head);
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
 }
