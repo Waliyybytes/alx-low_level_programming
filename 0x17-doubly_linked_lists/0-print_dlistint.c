48dd992691d1c62dc5fd4726042d4f14a6fc4b44
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-02 01:04:15 +0100

    clear print errors

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - to print list
+ * @h: head
+ * Return: number of elements
+ */
+
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
-	return i;
+	return (i);
 }
