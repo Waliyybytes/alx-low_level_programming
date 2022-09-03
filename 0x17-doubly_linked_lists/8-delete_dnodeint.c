e883b06323e3fbf52663bc20208b3b7ab4d0037a
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-03 12:44:02 +0100

    delet _node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,21 +17,21 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	tmp = *head;
-	tmp2 = *head;
+	tmp = *head, tmp2 = *head;
 	while (1)
 	{
-		tmp2 = tmp2->next;
-		count++;
+		tmp2 = tmp2->next, count++;
 		if (tmp2 == NULL)
 			break;
 	}
+	free(tmp2);
 	if (index == 0)
 	{
 		if ((*head)->next != NULL)
-			(*head)->next->prev = NULL, *head = (*head)->next;
+			(*head)->next->prev = NULL, *head = (*head)->next, free(tmp);
 		else
-			(*head) = NULL;
+		{	*head = NULL;
+			return (-1);}
 	}
 	if (index > 0 && index < count)
 	{
@@ -40,22 +40,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (tmp->next != NULL)
 				tmp = tmp->next;
 		}
-		dummy = tmp->next;
-		free(tmp->next);
-		dummy->next->prev = tmp;
+		dummy = tmp->next, free(tmp->next), dummy->next->prev = tmp;
 		tmp->next = dummy->next;
-
 	}
 	if (index == count)
 	{
 		while (tmp->next->next != NULL)
 			tmp = tmp->next;
+		free(tmp->next);
 		tmp->next = NULL;
-		tmp->next->prev = NULL;
-
 	}
 	if (index > count)
 		return (-1);
 	return (1);
 }
-
