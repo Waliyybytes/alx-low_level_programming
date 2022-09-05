1a201a5b22729fcb7910a9b3c4daa6db58bb7b6c
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-05 17:54:29 +0100

    autoscript

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c | gcc *.o -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
+gcc *.o -shared -o liball.so
