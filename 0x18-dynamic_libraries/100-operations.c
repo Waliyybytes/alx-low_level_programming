30b187e2e822d062889f43bfad3f80f782086643
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-05 19:50:06 +0100

    import_C_to_py

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+
+/**
+ * add - add two numbers
+ * @a: member
+ * @b: member
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: member
+ * @b: member
+ * Return: sum of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: member
+ * @b: member
+ * Return: sum of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: member
+ * @b: member
+ * Return: sum of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+ * mod - mod division two numbers
+ * @a: member
+ * @b: member
+ * Return: sum of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+
