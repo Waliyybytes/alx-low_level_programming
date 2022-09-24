794ac40f33d7ecd8f6dc5fec56b87f629b130da4
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-24 19:17:16 +0100

    hash_tabletask_1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return NULL;
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
+
