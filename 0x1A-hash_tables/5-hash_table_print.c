3f41446e718a4c32b891cf8820e106688768ebc3
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-09-25 14:55:12 +0100

    tasK_5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			printf("\'%s\': \'%s\'", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
 			node = node->next;
 			j++;
 		}
