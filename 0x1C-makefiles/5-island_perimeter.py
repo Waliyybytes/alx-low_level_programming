fc201e65846ac8e9a9a337b465a6e73d7b65faeb
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-11-10 22:10:26 +0100

    task_5 soln

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
 """
-Module Task
-
+    island_perimeter module
 """
 
 
 def island_perimeter(grid):
-    """Calculates the perimeter of a contiguous island"""
-    h, v = 0, 0
-    n = len(grid[0])
-    m = len(grid)
-    for i in range(1, m):
-        for j in range(1, n):
-            if grid[i - 1][j - 1:j + 2] in [[1, 1, 0], [0, 1, 1], [1, 1, 1]]:
-                h += 1
-    transposed = []
-    for i in range(m):
-        transposed.append([row[i] for row in grid])
-    for i in range(1, m):
-        for j in range(1, n):
-            if transposed[i - 1][j - 1:j + 2] in [
-                    [1, 1, 0], [0, 1, 1], [1, 1, 1]]:
-                v += 1
-    total = 0
-    for i in range(0, m):
-        for j in range(0, n):
-            total += grid[i][j]
-    if total == 1:
-        return 4
-    if h > 0 and v == 0:
-        h += 1
-    if v > 0 and h == 0:
-        v += 1
-    return 2 * (h + v)
+    """estimates the perimeter of an island"""
+    up, left, right, down = 0, 0, len(grid[0]), len(grid)
+    c = 0
+    for i in range(0, down):
+        for j in range(0, right):
+            if grid[i][j] == 1:
+                c += 1 if i == up or not grid[i - 1][j] else 0
+                c += 1 if j == left or not grid[i][j - 1] else 0
+                c += 1 if j == right - 1 or not grid[i][j + 1] else 0
+                c += 1 if i == down - 1 or not grid[i + 1][j] else 0
+    return c
